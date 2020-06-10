53b88ed31c362d7a1f3e04688818320090008a61
DipenMavaniLdce <62199316+DipenMavaniLdce@users.noreply.github.com>
2020-06-10 16:34:13 +0530

    pri

--- server/index.js
+++ server/index.js
@@ -1,11 +1,35 @@
-const app =require('express')()
+const express =require('express')
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://dipenMavaniMongoDb:bDognoMinavaMnepid@mern-aiftg.mongodb.net/merntest?retryWrites=true&w=majority',{useNewUrlParser:true})
+const app = express()
+const bodyParser = require('body-parser')
+const cookieParser =require('cookie-parser')
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.json())
+app.use(cookieParser())
+const config = require('./config/keys')
+const {User} = require('./model/user');
+
+mongoose.connect(config.mongoUri,{useNewUrlParser: true ,useUnifiedTopology: true })
         .then(() => console.log("object"))
         .catch(err => console.error(err))
+
+
+
 app.get('/',(req,res) => {
+    console.log("object")
     res.send("hey buddy wp")
+})
+
+app.post('/api/users/register',(req,res) => {
+    
+    const user = new User(req.body);
+    user.save((err, doc) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json(doc);
+    });
+   
 })
 
 
 app.listen(5000)
+
